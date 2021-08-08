56647f496fc817dac7a90a59d32b41fef5bd540d
VenkataSaichandNeeli <88621838+VenkataSaichandNeeli@users.noreply.github.com>
2021-08-08 20:02:27 +0530

    Create Multiply Numbers.java

---
+++ JAVA/Java Fundamentals/Multiply Numbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+  import java.io.*;
+  
+  public class Main {
+    public static void main(String args[]) throws IOException {
+      
+      //write your code here
+      Scanner in = new Scanner(System.in);
+      int x = in.nextInt();
+      int y = in.nextInt();
+      int res = x * y;
+      System.out.println(res);
+    }
+  }
+
