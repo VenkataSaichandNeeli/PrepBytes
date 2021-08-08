481c575478435a6a2a492ac3c0e830c9e4d45dea
VenkataSaichandNeeli <88621838+VenkataSaichandNeeli@users.noreply.github.com>
2021-08-08 19:59:59 +0530

    Create Print Floats Number.java

---
+++ JAVA/Input and Output/Print Floats Number.java
@@ -0,0 +1,14 @@
+import java.util.*;
+  import java.io.*;
+  
+  public class Main {
+    public static void main(String args[]) throws IOException {
+      
+      //write your code here
+      Scanner in = new Scanner(System.in);
+      float n = in.nextFloat();
+      float m = in.nextFloat();
+      System.out.printf("%.2f  %.2f",n,m);
+ 
+    }
+  }
