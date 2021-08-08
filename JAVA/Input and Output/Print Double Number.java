01c71d69b855a14ac4bfda5ecfc1888359f21b4b
VenkataSaichandNeeli <88621838+VenkataSaichandNeeli@users.noreply.github.com>
2021-08-08 20:00:34 +0530

    Create Print Double Number.java

---
+++ JAVA/Input and Output/Print Double Number.java
@@ -0,0 +1,13 @@
+import java.util.*;
+  import java.io.*;
+  
+  public class Main {
+    public static void main(String args[]) throws IOException {
+      
+      //write your code here
+      Scanner s = new Scanner(System.in);
+      double n = s.nextDouble();
+      System.out.printf("%.2f",n);
+      
+    }
+  }
