8369eeb3a27f96cac95adc1e0225163879fbeb56
VenkataSaichandNeeli <88621838+VenkataSaichandNeeli@users.noreply.github.com>
2021-08-08 20:03:42 +0530

    Create last Digit of  Number.java

---
+++ JAVA/Java Fundamentals/last Digit of  Number.java
@@ -0,0 +1,14 @@
+import java.util.*;
+  import java.io.*;
+  
+  public class Main {
+    public static void main(String args[]) throws IOException {
+      
+      //write your code here
+      Scanner in = new Scanner(System.in);
+      int y = in.nextInt();
+      int res = y % 10;
+      System.out.println(res);
+      
+    }
+  }
