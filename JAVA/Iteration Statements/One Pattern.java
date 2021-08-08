249b417c0938509e3ede0c9d5fd77630c3633ccf
VenkataSaichandNeeli <88621838+VenkataSaichandNeeli@users.noreply.github.com>
2021-08-08 20:29:28 +0530

    Create One Pattern.java

---
+++ JAVA/Iteration Statements/One Pattern.java
@@ -0,0 +1,20 @@
+import java.util.*;
+  import java.io.*;
+  
+  public class Main {
+    public static void main(String args[]) throws IOException {
+      
+      //write your code here
+      Scanner in = new Scanner(System.in);
+      int n = in.nextInt();
+      for(int i = 1;i <= 5;i++)
+      {
+        for(int j = 1;j <= i;j++)
+        {
+          System.out.print(n+" ");
+        }
+        System.out.println();
+      }
+      
+    }
+  }
