e1f32b7dac2bea79cd9761831947fdbb4d4bf413
VenkataSaichandNeeli <88621838+VenkataSaichandNeeli@users.noreply.github.com>
2021-08-08 19:59:22 +0530

    Create Mixed Input.java

---
+++ JAVA/Input and Output/Mixed Input.java
@@ -0,0 +1,15 @@
+import java.util.*;
+  import java.io.*;
+  
+  public class Main {
+    public static void main(String args[]) throws IOException {
+      
+      //write your code here
+      Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    float m = in.nextFloat();
+    String ch = in.next();
+    System.out.printf("%d$%.2f$%s",n,m,ch);
+      
+    }
+  }
