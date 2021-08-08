aef1fdcf9b4b723d53dfd7f402e0688b5fd904e4
VenkataSaichandNeeli <88621838+VenkataSaichandNeeli@users.noreply.github.com>
2021-08-08 19:58:34 +0530

    Create Print Character.java

---
+++ JAVA/Input and Output/Print Character.java
@@ -0,0 +1,13 @@
+import java.util.*;
+  import java.io.*;
+  
+  public class Main {
+    public static void main(String args[]) throws IOException {
+      
+      //write your code here
+      Scanner in = new Scanner(System.in);
+      String ch;
+      ch = in.next();
+      System.out.println(ch);
+    }
+  }
