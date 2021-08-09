4bcb701952a897ab635785861d9324d6e24b3b1d
VenkataSaichandNeeli <88621838+VenkataSaichandNeeli@users.noreply.github.com>
2021-08-09 20:18:04 +0530

    Create Saitams's Punch.java

---
+++ JAVA/Arrays/Saitams's Punch.java
@@ -0,0 +1,37 @@
+import java.util.*;
+  import java.io.*;
+  
+  public class Main {
+    public static void main(String args[]) throws IOException {
+      
+      //write your code here
+      Scanner in = new Scanner(System.in);
+      int t = in.nextInt();
+      while(t-->0)
+      {
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+          arr[i] = in.nextInt();
+        }
+        int l=0;
+        int sum = k;
+        for(int i = 0; i < n-1; i++)
+        {
+          if((arr[i+1]-arr[i]) == 0)
+            sum = sum;
+          else if(arr[i+1] - arr[i] >=k)
+          sum += k;
+          else
+          {
+          l = arr[i+1] - arr[i];
+          sum = sum + l;
+          }
+        }
+        System.out.println(sum);
+      }
+      
+    }
+  }
